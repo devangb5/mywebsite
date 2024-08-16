c40455018ab3d642d4c0869a1aad4224f68b54c7
devangb5 <devangb5@gmail.com>
2024-08-16 10:30:31 -0400

    push_test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,27 @@
-import { getMessaging, getToken } from "firebase/messaging";
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
+// firebase-messaging-sw.js
 
+importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging.js');
 
+firebase.initializeApp({
+    apiKey: "AIzaSyCPPrwO_kXf3g4LcXn1pQgv3ArdZITMljo",
+	  authDomain: "push-notificcations.firebaseapp.com",
+	  projectId: "push-notificcations",
+	  storageBucket: "push-notificcations.appspot.com",
+	  messagingSenderId: "48222894503",
+	  appId: "1:48222894503:web:079a49a0476ef594d79e82",
+	  measurementId: "G-8G3SJQ59JE"
+});
 
-// Get registration token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-const messaging = getMessaging();
-getToken(messaging, { vapidKey: 'BLYCHdTcgSGA4gBBEMFD-UC0SHeLfRJoANdmj3TFSh-aRmTbVq2UbLXZ2UP1ziVjV4CTxZHv5bYIiHthwljNCME' }).then((currentToken) => {
-  if (currentToken) {
-    // Send the token to your server and update the UI if necessary
-    // ...
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-    // ...
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: './wp-content/' // Customize your icon path
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
