2528feb2f9d1fd5dd4500cebd520402a5bc1fdc7
devangb5 <devangb5@gmail.com>
2024-08-16 10:06:27 -0400

    push_test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,51 +1,23 @@
-// firebase-messaging-sw.js
+import { getMessaging, getToken } from "firebase/messaging";
+importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
+importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
+importScripts('/__/firebase/init.js');
 
-importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging.js');
 
-firebase.initializeApp({
-    apiKey: "AIzaSyCPPrwO_kXf3g4LcXn1pQgv3ArdZITMljo",
-    authDomain: "push-notificcations.firebaseapp.com",
-    projectId: "push-notificcations",
-    storageBucket: "push-notificcations.appspot.com",
-    messagingSenderId: "48222894503",
-    appId: "1:48222894503:web:079a49a0476ef594d79e82",
-    
+
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging();
+getToken(messaging, { vapidKey: 'BLYCHdTcgSGA4gBBEMFD-UC0SHeLfRJoANdmj3TFSh-aRmTbVq2UbLXZ2UP1ziVjV4CTxZHv5bYIiHthwljNCME' }).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
 });
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/firebase-logo.png'
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-messaging.requestPermission()
-  .then(() => {
-    console.log('Notification permission granted.');
-    // Get the token
-    return messaging.getToken();
-  })
-  .then((token) => {
-    console.log('FCM Registration Token:', token);
-    // Send this token to your server to store it and use it for sending notifications
-  })
-  .catch((err) => {
-    console.log('Unable to get permission to notify.', err);
-  });
-  messaging.onTokenRefresh(() => {
-    messaging.getToken()
-      .then((refreshedToken) => {
-        console.log('Token refreshed:', refreshedToken);
-        // Save the new token to your server
-      })
-      .catch((err) => {
-        console.log('Unable to retrieve refreshed token ', err);
-      });
-  });
