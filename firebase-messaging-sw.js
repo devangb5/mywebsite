e06bdd7cf39e31da86d5e6bc996c97a20c294a6f
devangb5 <devangb5@gmail.com>
2024-08-16 10:41:10 -0400

    push_test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // firebase-messaging-sw.js
 
-importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js');
+
 importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging.js');
 
 firebase.initializeApp({
