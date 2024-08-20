27cf989e2bc05b94de17ed30692df9878b99afb8
devangb5 <devangb5@gmail.com>
2024-08-20 08:47:54 -0400

    push_test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,5 @@
-/**
- * @license
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
     apiKey: "AIzaSyCVeeFxPw1jF5f_rHyofYg0fOEXD3qivbw",
@@ -27,20 +10,21 @@ const firebaseConfig = {
     messagingSenderId: "311060497968",
     appId: "1:311060497968:web:feb247239ee93c067ccff5",
     measurementId: "G-HXG39MJW55"
-  };
+};
 
 firebase.initializeApp(firebaseConfig);
 
-// Retrieve firebase messaging
 const messaging = firebase.messaging();
 
+// Handle background messages
 messaging.onBackgroundMessage(payload => {
-  console.log('Received background message ', payload);
+    console.log('Received background message ', payload);
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
+    const notificationTitle = payload.notification.title || 'No Title';
+    const notificationOptions = {
+        body: payload.notification.body || 'No Body',
+        icon: payload.notification.icon || '/default-icon.png' // Ensure default icon path is valid
+    };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
