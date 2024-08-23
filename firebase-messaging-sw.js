544150dd597ded6fc5270c8dcd97ebbe6e9639ae
devangb5 <devangb5@gmail.com>
2024-08-23 09:36:35 -0400

    push_hide

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -25,13 +25,13 @@ messaging.onBackgroundMessage(payload => {
         body: payload.notification.body || 'No Body',
         icon: payload.notification.icon || 'https://aroundtheville.com/wp-content/uploads/2024/06/food-trucks-cover.webp', // Ensure default icon path is valid
         data:{
-        url : payload.notification.url || 'https://aroundtheville.com'}
+          url: payload.notification.data ? payload.notification.data.url : 'https://aroundtheville.com'}
       };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
 self.addEventListener('notificationclick', (event) => {
-  const url = event.notification.data.url || '/'; // Default to home if no URL
+  const url = event.notification.data.url || ''; // Default to home if no URL
 
   event.notification.close(); // Close the notification
 
